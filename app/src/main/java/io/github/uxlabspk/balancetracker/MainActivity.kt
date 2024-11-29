de56c4abecded3a9e8d42ec21e1e4731dc938a73
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2024-11-29 22:10:17 +0500

    Naveed: Refactoring some code

--- app/src/main/java/io/github/uxlabspk/balancetracker/MainActivity.kt
+++ app/src/main/java/io/github/uxlabspk/balancetracker/MainActivity.kt
@@ -1,25 +1,11 @@
 package io.github.uxlabspk.balancetracker
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import io.github.uxlabspk.balancetracker.ui.theme.BalanceTrackerTheme
 import io.github.uxlabspk.balancetracker.views.IntroductionPage
-import io.github.uxlabspk.balancetracker.views.components.PrimaryButton
-import io.github.uxlabspk.balancetracker.views.components.UserInput
-import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
